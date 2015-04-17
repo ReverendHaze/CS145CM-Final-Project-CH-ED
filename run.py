96163ffe970f81dfcea40b075ab2397a9fe28e8d
EDwyer <Eleanor.A.Dwyer@gmail.com>
2015-04-17 16:18:10 -0700

    merged feature-selection with devel

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import os
 import tweet_df
 import graph_module
 import cluster_module
+import ngram_module
 
 # Code to execute when the script is run
 def main():
@@ -28,6 +29,41 @@ def main():
     # three desired cities from the dataset.
     master_df = master_df[master_df['City'] != 'Other']
 
+    #function to get dictionary of histograms for a given city and time window
+    def get_histograms(city, n_days=0, n_hours=1, n_minutes=0):
+
+        master_df = master_df.loc[(master_df['City']==city), 'text']
+
+        t_step = datetime.timedelta(days=n_days, minutes=n_minutes, hours=n_hours)
+
+        # create empty dictionary
+        histograms = {}
+
+        # loop over master_df by timestep
+        t_start = master_df.DatetimeIndex.min()
+        t_max = master_df.DatetimeIndex.max()
+
+        while t_start <= t_max:
+            t_end = t_start + t_step
+
+            # get dictionary of bigrams and their frequency within the timestep
+            freq_dict = gram_module.function(master_df[master_df.index < t_end])
+
+            # remove current timestep from master_df and update end of next timestep
+            master_df = master_df[master_df.index >= t_start]
+            t_start = t_end
+
+            # add that timestep's output to main histogram dictionary
+            for key, value in freq_dict.iteritems():
+                if key in histograms:
+                    histograms[key] = histograms[key].append(value)
+                else:
+                    histograms[key] = np.array([value])
+
+        #return completed dictionary of histogram vectors
+        return histograms
+
+
     for index, df in master_df.groupby('City'):
         # Graph tweet rate over time
         graph_module.GraphFreqs(df, city=index)
@@ -36,6 +72,7 @@ def main():
         centers = cluster_module.ApplyKMeans(df, ['longitude', 'latitude'], 6)
         graph_module.GraphClusteredHexbin(df, centers, index)
     return master_df
+
 
 # Helper function to make the directory
 def mkdir(folder):
