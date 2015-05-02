23aef148526e7e5a18162b7c562ac97daaa7b25c
EDwyer <Eleanor.A.Dwyer@gmail.com>
2015-05-01 21:38:37 -0700

    Update nmf.py

--- nmf.py
+++ nmf.py
@@ -1,18 +1,16 @@
 import pandas as pd
 from sklearn.decomposition import NMF
 
-n_samples = 2000
-n_features = 1000
-n_topics = 10
+n_topics = 50 
 n_top_words = 20
 
-in_file =
+in_file = 
 
 hist = pd.pickle.io.read_pickle(in_file)
 
 nmf = NMF(n_components=n_topics, random_state=1).fit(hist)
 
-feature_names = vectorizer.get_feature_names()
+feature_names = hist.columns
 
 for topic_idx, topic in enumerate(nmf.components_):
     print("Topic #%d:" % topic_idx)
